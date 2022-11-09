86f4eb489ca7ecdfb66f1b5d035f308ef820ea98
ANU J PILLAI <107497512+BEAST-0@users.noreply.github.com>
2022-11-09 13:13:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,20 @@ const cors = require("cors");
 const user = require("./routes/userroute");
 const book = require("./routes/bookroute");
 const app = new express();
-<<<<<<< HEAD
-=======
+
 const path = require("path");
 
 const port = process.env.PORT || 8080;
->>>>>>> c25a21ff1100e5d0e033d67856690385cc8c1774
 
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 8080;
-
-<<<<<<< HEAD
-app.listen("port", () => {
-  console.log("Server started"= port);
-=======
-app.use('/api', user);
-app.use('/api', book);
+app.use("/user", user);
+app.use("/book", book);
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
->>>>>>> c25a21ff1100e5d0e033d67856690385cc8c1774
 });
 
-app.use("/users", user);
-app.use("/books", book);
+app.listen("port", () => {
+  console.log("Server started" + port);
+});
