948eccdd91c6824317b4b35e3db39075e3466ba9
ANU J PILLAI <107497512+BEAST-0@users.noreply.github.com>
2022-11-09 13:03:57 +0530

    update

--- app.js
+++ app.js
@@ -3,22 +3,15 @@ const cors = require("cors");
 const user = require("./routes/userroute");
 const book = require("./routes/bookroute");
 const app = new express();
-const path = require("path");
-
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("./dist/frontend"));
+const port = process.env.PORT || 8080;
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
+app.listen("port", () => {
+  console.log("Server started"= port);
 });
 
-app.listen(port, () => {
-  console.log("server is connected:", port);
-});
-
-app.use("/api/users", user);
-app.use("/api/books", book);
+app.use("/users", user);
+app.use("/books", book);
